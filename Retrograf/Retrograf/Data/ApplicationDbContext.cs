190f6b096424f19f9840ba1eedb34cd480cddcdc
HenzoWTF <Urenahenzo@gmail.com>
2024-04-22 16:30:26 -0400

    Colocacion de las calses en el ApplicationDbContext

--- Retrograf/Retrograf/Data/ApplicationDbContext.cs
+++ Retrograf/Retrograf/Data/ApplicationDbContext.cs
@@ -1,9 +1,15 @@
+using Clases;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Retrograf.Data
+namespace Retrograf.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Compra> Compras { get; set; }
+    public DbSet<Facturas> Facturas { get; set; }
+    public DbSet<Productos> Productos { get; set; }
+    public DbSet<Ventas> Ventas { get; set; }
+
 }
