bc7cc90ca28e7f84e9bb5ad9bb4072f8721dd5cc
HenzoWTF <Urenahenzo@gmail.com>
2024-04-22 16:25:55 -0400

    Clase compra creada

---
+++ Clases/CompraDetalle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases;
+public class CompraDetalle
+{
+    [Key]
+    public int CompraDetalleId { get; set; }
+    public int ProductoId { get; set; }
+    [Range(0, 1000000, ErrorMessage = "El campo {0} debe ser mayor que 0 y menor que 1000000.")]
+    public int Cantidad { get; set; }
+    public float PrecioCompra { get; set; }
+}
