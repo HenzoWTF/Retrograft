851156fbc5a007f432ba3eeb1fce8ba898d8aeab
HenzoWTF <Urenahenzo@gmail.com>
2024-05-13 10:16:06 -0400

    Clase Carrito realizado

---
+++ Clases/Carrito.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Carrito
+    {
+        [Key]
+        public int IdCarrito { get; set; }
+
+        public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public float Precio { get; set; }
+        public float Total { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int Cantidad { get; set; }
+        public bool Realizado { get; set; }
+
+
+    }
+}
