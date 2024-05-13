5edec8c765b88795321eb6dfb76c7a5282dcae99
HenzoWTF <Urenahenzo@gmail.com>
2024-05-13 10:18:35 -0400

    Colocacion del services en el program

--- Retrograf/Retrograf/Program.cs
+++ Retrograf/Retrograf/Program.cs
@@ -40,6 +40,7 @@ builder.Services.AddScoped<ProductosServices>();
 builder.Services.AddScoped<VentasServices>();
 builder.Services.AddScoped<CuadreServices>();
 builder.Services.AddScoped<ClienteServices>();
+builder.Services.AddScoped<CarritoServices>();
 
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
